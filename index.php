248411c73253b883d42b1d849f141198e8285c9d
simanet <5601510+simanet@users.noreply.github.com>
2021-04-01 16:08:57 +0430

    update

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
-$webgate_id = 50000000; // شناسه درگاه
+//توکن درگاه ایرپول
+$token = '8f90d8b359c6bd1a11973c7b3c2803bb';
+$test_mode = true;
 
 ini_set("display_errors", 1);
 error_reporting(E_ALL); 
 
-if(isset($_POST['start_pay'])){ //خرید
+//خرید
+if(isset($_POST['start_pay'])){ 
 	$parameters = array( 
-		'webgate_id' 	=> $webgate_id, 			// Required (20 = Test Mode)
+		'method' 		=> 'payment', 				// Required
 		'amount' 		=> $_POST['amount'], 		// Required - (Rial)
 		'callback_url' 	=> $_POST['callback_url'], 	// Required
-		'plugin' 		=> 'other',					// Optional
 		'order_id' 		=> $_POST['order_id'], 		// Optional
 		'product' 		=> $_POST['product'],		// Required
 		'payer_name' 	=> $_POST['payer_name'],	// Required
@@ -19,19 +21,24 @@ if(isset($_POST['start_pay'])){ //خرید
 		'mobile' 		=> $_POST['mobile'],		// Required
 		'email'			=> $_POST['email'],			// Optional
 		'address' 		=> $_POST['address'],		// Optional
-		'description' 	=> $_POST['description']	// Optional
+		'description' 	=> $_POST['description'],	// Optional
+		'test_mode' 	=> $test_mode				// Optional
 	);
+	
+	$result 	= post_data('https://irpul.ir/ws.php', $parameters, $token );
 
-	try {
-		$client = new SoapClient('https://irpul.ir/webservice.php?wsdl' , array('soap_version'=>'SOAP_1_1','cache_wsdl'=>WSDL_CACHE_NONE  ,'encoding'=>'UTF-8'));
-		$result = $client->Payment($parameters);
-		//print_r($result);
-	}catch (Exception $e) { echo 'Error'. $e->getMessage();  }
+	if( isset($result['http_code']) ){
+		$data =  json_decode($result['data'],true);
 
-	if( isset($result) && $result['res_code']===1 ){
-		header('Location: '.$result['url']);
-	}else {
-		echo "Error Code: ".$result['res_code'] . ' ' . $result['status'];
+		if( isset($data['code']) && $data['code'] === 1){
+			header("Location: " . $data['url']);
+			exit;
+		}
+		else{
+			echo "Error Code: ".$data['code'] . ' ' . $data['status'];
+		}
+	}else{
+		echo 'پاسخی از سرویس دهنده دریافت نشد. لطفا دوباره تلاش نمائید';
 	}
 }
 elseif( isset($_GET['callback']) && isset($_GET['irpul_token']) && $_GET['irpul_token']!=''  ){ //تائید تراکنش
@@ -47,27 +54,31 @@ elseif( isset($_GET['callback']) && isset($_GET['irpul_token']) && $_GET['irpul_
 		
 		if($status == 'paid'){
 			$parameters = array	(
-				'webgate_id'	=> $webgate_id,	// Required
-				'tran_id' 		=> $tran_id,	// Required
+				'method' 	    => 'verify',
+				'trans_id' 		=> $tran_id,	// Required
 				'amount'	 	=> $amount		// Required
 			);
-			try {
-				$client = new SoapClient('http://irpul.ir/webservice.php?wsdl' , array('soap_version'=>'SOAP_1_1','cache_wsdl'=>WSDL_CACHE_NONE ,'encoding'=>'UTF-8'));
-				$result = $client->PaymentVerification($parameters);
-			}catch (Exception $e) { echo 'Error'. $e->getMessage();  }
+			
+			$result =  post_data('https://irpul.ir/ws.php', $parameters, $token );
 
-			if ($result == 1){
-				echo "transaction paid. refcode :" .$refcode;
-			}
-			else{
-				echo 'Error Code: '.$result;
-				echo "<br/><a href='http://irpul.ir/webservice/' target='_blank' >Help</a>";
+			if( isset($result['http_code']) ){
+				$data =  json_decode($result['data'],true);
+
+				if( isset($data['code']) && $data['code'] === 1){
+					echo "transaction paid. refcode :" .$refcode;
+				}
+				else{
+					echo 'خطا در پرداخت. کد خطا: ' . $data['code'] . '<br/>' . $data['status'];
+					echo "<br/><a href='http://irpul.ir/webservice/' target='_blank' >Help</a>";
+				}
+			}else{
+				echo 'پاسخی از سرویس دهنده دریافت نشد. لطفا دوباره تلاش نمائید';
 			}
 		}else{
-			echo "transaction not paid yet";
+			echo "تراکنش پرداخت نشده است";
 		}
 	}else{
-		echo "token invalid";
+		echo "توکن اشتباه است";
 	}
 }
 else{
@@ -113,7 +124,7 @@ WebService:
 		</tr>
 		<tr>
 			<th>Callback URL</th>
-			<td><input type=" text" name="callback_url" value="http://pay.inax.ir/index.php?callback" /></td>
+			<td><input type=" text" name="callback_url" value="http://127.0.0.1/sample_code/index.php?callback" /></td>
 		</tr>
 		<tr>
 			<th></th>
@@ -145,6 +156,70 @@ function url_decrypt($string){
 		return array('data'=>'' , 'status'=>false);
 	}
 }
+
+function post_data($url,$params,$token) {
+	ini_set('default_socket_timeout', 15);
+
+	$headers = array(
+		"Authorization: token= {$token}",
+		'Content-type: application/json'
+	);
+
+	$handle = curl_init($url);
+	curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 30);
+	curl_setopt($handle, CURLOPT_TIMEOUT, 40);
+
+	curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($params) );
+	curl_setopt($handle, CURLOPT_HTTPHEADER, $headers );
+
+	$response = curl_exec($handle);
+	//error_log('curl response1 : '. print_r($response,true));
+
+	$msg='';
+	$http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
+
+	$status= true;
+
+	if ($response === false) {
+		$curl_errno = curl_errno($handle);
+		$curl_error = curl_error($handle);
+		$msg .= "Curl error $curl_errno: $curl_error";
+		$status = false;
+	}
+
+	curl_close($handle);//dont move uppder than curl_errno
+
+	if( $http_code == 200 ){
+		$msg .= "Request was successfull";
+	}
+	else{
+		$status = false;
+		if ($http_code == 400) {
+			$status = true;
+		}
+		elseif ($http_code == 401) {
+			$msg .= "Invalid access token provided";
+		}
+		elseif ($http_code == 502) {
+			$msg .= "Bad Gateway";
+		}
+		elseif ($http_code >= 500) {// do not wat to DDOS server if something goes wrong
+			sleep(2);
+		}
+	}
+
+	$res['http_code'] 	= $http_code;
+	$res['status'] 		= $status;
+	$res['msg'] 		= $msg;
+	$res['data'] 		= $response;
+
+	if(!$status){
+		//error_log(print_r($res,true));
+	}
+	return $res;
+}
+
 
 ?>
 
